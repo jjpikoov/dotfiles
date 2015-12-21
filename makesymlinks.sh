d44fa2f31e6807a7cecf648f9234cf51ca517962
Pawel Jankowski <jjpikoov@gmail.com>
2015-12-22 00:27:41 +0100

    Openbox config added + modyfiactions in script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,26 +6,36 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+dotfiles=~/dotfiles                    # dotfiles directory
+olddotfiles=~/dotfiles_old             # old dotfiles backup directory
+files="bashrc Xdefaults gitconfig tmux.conf xinitrc"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+echo "Creating $olddotfiles for backup of any existing dotfiles in ~"
+mkdir -p $olddotfiles
 echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
+echo "Changing to the $dotfiles directory"
+cd $dotfiles
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
+    echo "Moving any existing dotfiles from ~ to $olddotfiles"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dotfiles/$file ~/.$file
+done
+
+#stuff for openbox
+echo "Creating copy of current openbox config"
+mkdir -p $olddotfiles/config/openbox
+mv ~/.config/openbox/* $olddotfiles/config/openbox 
+echo "Creating symlinks"
+obfiles="autostart menu.xml rc.xml"
+for file in $obfiles; do
+    ln -s $dotfiles/config/openbox/$file ~/.config/openbox/$file
 done
